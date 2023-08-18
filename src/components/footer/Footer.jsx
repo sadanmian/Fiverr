3fae4c50f10fd7f997e61672677e37ddfdf2323c
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-18 22:19:03 +0530

    React Router DOM

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./footer.scss";
+
+export const Footer = () => {
+  return <div className="footer">Footer</div>;
+};
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+  const { pathname } = useLocation();
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -24,13 +25,13 @@ const Navbar = () => {
   };
 
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          {/* <Link to={"/"}> */}
-          <span className="text">fiverr</span>
-          <span className="dot">.</span>
-          {/* </Link> */}
+          <Link to={"/"} className="link">
+            <span className="text">fiverr</span>
+            <span className="dot">.</span>
+          </Link>
         </div>
         <div className="links">
           <span>Fiverr Business</span>
@@ -51,13 +52,23 @@ const Navbar = () => {
                   <div className="options">
                     {currentUser.isSeller && (
                       <>
-                        <span>Gigs</span>
-                        <span>Add New Gig</span>
+                        <Link className="link" to={"/mygigs"}>
+                          Gigs
+                        </Link>
+                        <Link className="link" to={"/add"}>
+                          Add New Gig
+                        </Link>
                       </>
                     )}
-                    <span>Orders</span>
-                    <span>Messages</span>
-                    <span>Logout</span>
+                    <Link className="link" to={"/orders"}>
+                      Orders
+                    </Link>
+                    <Link className="link" to={"/messages"}>
+                      Messages
+                    </Link>
+                    <Link className="link" to={"/"}>
+                      Logout
+                    </Link>
                   </div>
                 )}
               </div>
@@ -65,12 +76,37 @@ const Navbar = () => {
           )}
         </div>
       </div>
-      {active && (
+      {(active || pathname) !== "/" && (
         <>
           <hr />
           <div className="menu">
-            <span>Test</span>
-            <span>Test 2</span>
+            <Link className="link" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link" to="/">
+              AI Services
+            </Link>
+            <Link className="link" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link" to="/">
+              Business
+            </Link>
+            <Link className="link" to="/">
+              Lifestyle
+            </Link>
           </div>
         </>
       )}
