01f0fc6ee62b65d4b290016f2ea423cc87d0b163
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-19 00:07:13 +0530

    Slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./home.scss";
 import { Featured } from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
   return (
     <div>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((item) => (
+          <CatCard key={item.id} item={item} />
+        ))}
+      </Slide>
     </div>
   );
 };
