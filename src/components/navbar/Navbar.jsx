36b03a16c153b5f41a8f0b68fde92bc1f985da04
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-18 15:55:16 +0530

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Link to={"/"}> */}
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+          {/* </Link> */}
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test</span>
+        <span>Test 2</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
