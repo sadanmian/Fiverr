c10e85f427e4e04a2170c25d918d0f30cf1ca083
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-19 23:00:03 +0530

    Gig items

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,8 +1,118 @@
 import React from "react";
 import "./gig.scss";
+import { Slider } from "infinite-react-carousel";
 
 const Gig = () => {
-  return <div>Gig</div>;
+  return (
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">FIVERR > GRAPHICS & DESIGN ></span>
+          <h1>I will create AI generated art for you</h1>
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <span>John</span>
+            <div className="stars">
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <Slider slideToShow={1} arrowsScroll={1}>
+            <img
+              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+          </Slider>
+          <h2>About This Gig</h2>
+          <p>
+            I use an AI program to create images based on text prompts. This
+            means I can help you to create a vision you have through a textual
+            description of your scene without requiring any reference images.
+            Some things I've found it often excels at are: Character portraits
+            (E.g. a picture to go with your DnD character) Landscapes (E.g.
+            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
+            team, business, profile picture) You can be as vague or as
+            descriptive as you want. Being more vague will allow the AI to be
+            more creative which can sometimes result in some amazing images. You
+            can also be incredibly precise if you have a clear image of what you
+            want in mind. All of the images I create are original and will be
+            found nowhere else. If you have any questions you're more than
+            welcome to send me a message.
+          </p>
+          <div className="seller">
+            <h2>About the Seller</h2>
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="info">
+                <span>John</span>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
+                </div>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
+                </div>
+                <div className="item">
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
+                </div>
+              </div>
+              <hr />
+              <p>
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Gig;
