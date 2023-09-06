676a908720d57538c64f55cc8abebf9c88ad5bfe
Sadan Mian <sadanmiyan007@gmail.com>
2023-09-06 21:22:48 +0530

    Slider

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -14,7 +14,9 @@ const Gigs = () => {
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN ></span>
+        <span className="breadcrumbs">
+          FIVERR {">"} GRAPHICS & DESIGN {">"}
+        </span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Fiverr's AI artists
